8a593b180e188c3ff07d06010d5272fc22582a87
Leo Liang <liangjinhua@gmail.com>
2014-02-13 14:00:11 +0800

    initial commit for v2

--- zebra-client/src/main/java/com/dianping/zebra/parser/sqlParser/groupFunction/Min.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.zebra.parser.sqlParser.groupFunction;
-
-public class Min extends CommonOneColumnFunction implements GroupFunction {
-	@Override
-	public String getFunctionName() {
-		return "min";
-	}
-}
--- zebra-client/src/main/java/com/dianping/zebra/parser/sqlParser/groupFunction/Sum.java
+++
@@ -1,9 +0,0 @@
-package com.dianping.zebra.parser.sqlParser.groupFunction;
-
-public class Sum extends CommonOneColumnFunction implements GroupFunction {
-	@Override
-	public String getFunctionName() {
-		return "sum";
-	}
-
-}
