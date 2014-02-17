1453702ef8649a3025daae5ddb7bddd396e07feb
胡鹏 <hupenghp2005@gmail.com>
2014-02-17 10:31:12 +0800

    add interface

--- zebra-client/src/main/java/com/dianping/zebra/group/healthcheck/HealthCheck.java
+++ zebra-client/src/main/java/com/dianping/zebra/group/healthcheck/HealthCheck.java
@@ -1,41 +1,7 @@
 package com.dianping.zebra.group.healthcheck;
 
-import java.util.List;
+public interface HealthCheck {
 
-import com.dianping.zebra.group.config.ConfigChangeEvent;
-import com.dianping.zebra.group.config.ConfigChangeListener;
-
-public class HealthCheck implements ConfigChangeListener{
-	private int heartbeatTime;
+	public void healthProblemNotify(String dsKey,Throwable e);
 	
-	private List<String> activeDsKeys;
-	
-	private List<String> unactiveDsKeys;
-	
-	
-	public void healthProblemNotify(String dsKey,Throwable e){
-	
-	}
-
-
-	@Override
-   public void onChange(ConfigChangeEvent event) {
-	   // TODO Auto-generated method stub
-	   
-   }
-	
-	
-	private class heartbeatCheck implements Runnable{
-
-		@Override
-      public void run() {
-	      // TODO Auto-generated method stub
-	      // check unactive ds.
-	
-			
-			
-      }
-		
-	}
-
 }
