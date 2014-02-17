086973d4076204e4846da577574c4b7ada15d371
ainilife <ainilife@gmail.com>
2014-02-17 20:31:21 +0800

    refine interface

--- zebra-client/src/main/java/com/dianping/zebra/group/manager/GroupDataSourceManagerFactory.java
+++ zebra-client/src/main/java/com/dianping/zebra/group/manager/GroupDataSourceManagerFactory.java
@@ -1,9 +1,11 @@
 package com.dianping.zebra.group.manager;
 
+import com.dianping.zebra.group.config.GroupConfigManager;
+
 
 public class GroupDataSourceManagerFactory {
 
-	public static GroupDataSourceManager getGroupDataSourceManger(String datasource) {
+	public static GroupDataSourceManager getGroupDataSourceManger(GroupConfigManager groupConfigManager) {
 	   // TODO Auto-generated method stub
 	   return null;
    }
