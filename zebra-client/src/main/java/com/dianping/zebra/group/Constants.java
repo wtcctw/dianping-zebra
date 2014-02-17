9d929275d6f727ca8990a99588fb598be9168848
Leo Liang <liangjinhua@gmail.com>
2014-02-17 17:10:00 +0800

    add datasource

---
+++ zebra-client/src/main/java/com/dianping/zebra/group/Constants.java
@@ -0,0 +1,15 @@
+/**
+ * Project: zebra-client
+ * 
+ * File Created at Feb 17, 2014
+ * 
+ */
+package com.dianping.zebra.group;
+
+/**
+ * @author Leo Liang
+ * 
+ */
+public class Constants {
+	public static final String DEFAULT_CONFIG_MANAGER_TYPE = "liger";
+}
