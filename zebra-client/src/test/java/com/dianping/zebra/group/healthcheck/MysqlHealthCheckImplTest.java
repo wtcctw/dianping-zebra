4f3a1b2ea7723f2b00aa844552ca5b85bafafb5d
胡鹏 <hupenghp2005@gmail.com>
2014-02-20 17:12:27 +0800

    add healthcheck unit test

---
+++ zebra-client/src/test/java/com/dianping/zebra/group/healthcheck/MysqlHealthCheckImplTest.java
@@ -0,0 +1,50 @@
+package com.dianping.zebra.group.healthcheck;
+
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.dianping.zebra.group.config.GroupConfigManager;
+import com.dianping.zebra.group.config.LocalXmlConfigManager;
+import com.dianping.zebra.group.router.GroupDataSourceTarget;
+
+public class MysqlHealthCheckImplTest {
+	private GroupConfigManager configManager;
+
+	@Before
+	public void init() {
+		String resourceId = "datasources.xml";
+		this.configManager = new LocalXmlConfigManager(resourceId);
+		configManager.init();
+	}
+	
+	@Test
+	public void testReadNotifyException(){
+		MysqlHealthCheckImpl mysqlhealthcheckimpl = new MysqlHealthCheckImpl(this.configManager);
+		GroupDataSourceTarget dsTarget = new GroupDataSourceTarget("db1", 1, true);
+		SQLException e = new SQLException();
+		int i = mysqlhealthcheckimpl.getMaxErrorTimes() + 1;
+		while(i > 0 ){
+			mysqlhealthcheckimpl.notifyException(dsTarget, e);
+			i--;
+		}
+		Assert.assertEquals(this.configManager.getAvailableDataSources().size(), 1);
+		Assert.assertEquals(this.configManager.getUnAvailableDataSources().size(), 1);
+	}
+	
+	@Test
+	public void testWriteNotifyException(){
+		MysqlHealthCheckImpl mysqlhealthcheckimpl = new MysqlHealthCheckImpl(this.configManager);
+		GroupDataSourceTarget dsTarget = new GroupDataSourceTarget("db1", 1, false);
+		SQLException e = new SQLException();
+		int i = mysqlhealthcheckimpl.getMaxErrorTimes() + 1;
+		while(i > 0 ){
+			mysqlhealthcheckimpl.notifyException(dsTarget, e);
+			i--;
+		}
+		Assert.assertEquals(this.configManager.getAvailableDataSources().size(), 2);
+		Assert.assertEquals(this.configManager.getUnAvailableDataSources().size(), 0);
+	}
+}
