33df67a0b98e774862fc96ee8fad29cdcf3384dd
atellwu <atell@qq.com>
2014-02-21 14:22:29 +0800

    去掉zebra-cleint中的filter和加密util

--- zebra-client/src/main/java/com/dianping/zebra/group/router/GroupDataSourceContext.java
+++ zebra-client/src/main/java/com/dianping/zebra/group/router/GroupDataSourceContext.java
@@ -1,20 +1,11 @@
 package com.dianping.zebra.group.router;
 
 import java.io.Serializable;
+import java.lang.reflect.Method;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dianping.avatar.tracker.TrackerContext;
-import com.dianping.dpsf.ContextUtil;
-
-/**
- * 
- * TODO 放在哪个auth类里（与登录标识一样）？
- * 
- * @author atell
- * 
- */
 public class GroupDataSourceContext implements Serializable {
 	private static final String CONTEXT_NAME = "com.dianping.zebra.group.router.GroupDataSourceContext";
 
@@ -26,9 +17,40 @@ public class GroupDataSourceContext implements Serializable {
 
 	private static GroupDataSourceContext dsContext;
 
+	private static Class<?> trackerContextClass;
+
+	private static Method getContextMethod;
+
+	private static Method setContextMethod;
+
+	private static Method getExtensionMethod;
+
+	private static Method addExtensionMethod;
+
 	private boolean forceReadFromMasterByCookie = false;
 
 	private boolean forceReadFromMaster = false;
+
+	static {
+		try {
+			Class<?> contextHolderClass = Class.forName("com.dianping.dpsf.ContextUtil");
+			trackerContextClass = Class.forName("com.dianping.avatar.tracker.TrackerContext");
+
+			getContextMethod = contextHolderClass.getDeclaredMethod("getContext", new Class[] {});
+			setContextMethod = contextHolderClass.getDeclaredMethod("setContext", new Class[] { trackerContextClass });
+			getExtensionMethod = trackerContextClass.getDeclaredMethod("getExtension", new Class[] { String.class });
+			addExtensionMethod = trackerContextClass.getDeclaredMethod("addExtension", new Class[] { String.class });
+
+			getContextMethod.setAccessible(true);
+			setContextMethod.setAccessible(true);
+			getExtensionMethod.setAccessible(true);
+			addExtensionMethod.setAccessible(true);
+
+		} catch (Exception e) {
+			logger.warn("No App context: " + e.getMessage(), e);
+		}
+
+	}
 
 	public static GroupDataSourceContext get() {
 		dsContext = threadLocalContext.get();
@@ -49,23 +71,25 @@ public class GroupDataSourceContext implements Serializable {
 
 	private static GroupDataSourceContext getContextFromDpsf() {
 		GroupDataSourceContext _dsContext = null;
-		try {
-			Class.forName("com.dianping.dpsf.ContextUtil");// 检查是否依赖了dpsf，如果没有依赖，则忽略
 
-			TrackerContext trackerContext = (TrackerContext) ContextUtil.getContext();
-			if (trackerContext == null) {
-				trackerContext = new TrackerContext();
-				ContextUtil.setContext(trackerContext);
-			}
-			_dsContext = (GroupDataSourceContext) trackerContext.getExtension(CONTEXT_NAME);
-			if (_dsContext == null) {
-				_dsContext = new GroupDataSourceContext();
-				trackerContext.addExtension(CONTEXT_NAME, _dsContext);
-			}
+		if (trackerContextClass != null) {
+			try {
+				Object trackerContext = getContextMethod.invoke(null);
 
-		} catch (ClassNotFoundException e) {
-		} catch (Exception e) {
-			logger.warn("Error in dpsf, ignore TrackerContext from dpsf", e);
+				if (trackerContext == null) {
+					trackerContext = trackerContextClass.newInstance();
+					setContextMethod.invoke(null, trackerContext);
+				}
+				_dsContext = (GroupDataSourceContext) getExtensionMethod.invoke(trackerContext, CONTEXT_NAME);
+
+				if (_dsContext == null) {
+					_dsContext = new GroupDataSourceContext();
+					addExtensionMethod.invoke(trackerContext, CONTEXT_NAME);
+				}
+
+			} catch (Exception e) {
+				logger.warn("Error in dpsf, ignore TrackerContext from dpsf", e);
+			}
 		}
 
 		return _dsContext;
