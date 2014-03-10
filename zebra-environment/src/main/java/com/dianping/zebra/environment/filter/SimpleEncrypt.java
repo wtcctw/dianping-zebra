e5e7382c5d647dacf7ca040ae0c6f9df9538bdb8
atellwu <atell@qq.com>
2014-03-10 22:11:19 +0800

    修改混淆cookie的加密算法，给字节加上校验字节。不使用字符串，这样减少长度。

--- zebra-environment/src/main/java/com/dianping/zebra/environment/filter/SimpleEncrypt.java
+++ zebra-environment/src/main/java/com/dianping/zebra/environment/filter/SimpleEncrypt.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class SimpleEncrypt {
 
-	private static final int RADIX = 16;
+	private static final int RADIX = 36;
 
 	private final BigInteger bigIntSeed;
 
@@ -18,38 +18,86 @@ public class SimpleEncrypt {
 		BigInteger encryptedBigInt = new BigInteger(value, RADIX);
 		BigInteger decrytedBigInt = encryptedBigInt.xor(bigIntSeed);
 
-		long changedHighLow = Long.valueOf(new String(decrytedBigInt.toByteArray(), "UTF-8"));
+		long changedHighLow = bytesToLong(decrytedBigInt.toByteArray());
+
 		// 高低交换
-		long high = changedHighLow >>> 32;
-		long timestamp = (changedHighLow << 32) | high;
+		long timestamp = changeHighLow(changedHighLow);
 
 		return timestamp;
 	}
 
-	public String encryptTimestamp(long timestamp) throws UnsupportedEncodingException {
-		// 高低交换
-		long high = timestamp >>> 32;
-		long changedHighLow = (timestamp << 32) | high;
+	private long changeHighLow(long changedHighLow) {
+		long high = changedHighLow >>> 32;
+		long timestamp = (changedHighLow << 32) | high;
+		return timestamp;
+	}
 
-		String text = String.valueOf(changedHighLow);
-		BigInteger textBigInt = new BigInteger(text.getBytes("UTF-8"));
+	public String encryptTimestamp(long timestamp) throws UnsupportedEncodingException {
+		long changedHighLow = changeHighLow(timestamp);
+
+		BigInteger textBigInt = new BigInteger(longToBytes(changedHighLow));
 
 		BigInteger encrypted = bigIntSeed.xor(textBigInt);
 
 		return encrypted.toString(RADIX);
 	}
 
+	private byte[] longToBytes(long v) {
+		byte[] writeBuffer = new byte[12];
+
+		writeBuffer[0] = (byte) (v >>> 56);
+		writeBuffer[1] = (byte) (v >>> 48);
+		// 用于校验的字节
+		writeBuffer[2] = (byte) (writeBuffer[0] ^ writeBuffer[1]);
+
+		writeBuffer[3] = (byte) (v >>> 40);
+		writeBuffer[4] = (byte) (v >>> 32);
+		// 用于校验的字节
+		writeBuffer[5] = (byte) (writeBuffer[3] ^ writeBuffer[4]);
+
+		writeBuffer[6] = (byte) (v >>> 24);
+		writeBuffer[7] = (byte) (v >>> 16);
+		// 用于校验的字节
+		writeBuffer[8] = (byte) (writeBuffer[6] ^ writeBuffer[7]);
+
+		writeBuffer[9] = (byte) (v >>> 8);
+		writeBuffer[10] = (byte) (v >>> 0);
+		// 用于校验的字节
+		writeBuffer[11] = (byte) (writeBuffer[9] ^ writeBuffer[10]);
+
+		return writeBuffer;
+	}
+
+	private long bytesToLong(byte[] byteArray) {
+		long val = 0;
+		byte[] checkBytes = new byte[2];
+		for (int i = 0; i < byteArray.length; i++) {
+			if (i % 3 == 2) {
+				// 校验
+				if (byteArray[i] != (byte) (checkBytes[0] ^ checkBytes[1])) {
+					throw new IllegalArgumentException("Illegal encrypted value");
+				}
+			} else {
+				val <<= 8;
+				val |= (byteArray[i] & 0x00FF);
+
+				checkBytes[i % 2] = byteArray[i];
+			}
+		}
+		return val;
+	}
+
 	public static void main(String args[]) throws UnsupportedEncodingException {
 		long timestamp = System.currentTimeMillis();
 		System.out.println(timestamp);
-		SimpleEncrypt simpleEncrypt = new SimpleEncrypt(312989518543887L);
+		SimpleEncrypt simpleEncrypt = new SimpleEncrypt(8543887L);
 		String entrypt = simpleEncrypt.encryptTimestamp(timestamp);
 		System.out.println(entrypt);
 		long dec = simpleEncrypt.decryptTimestamp(entrypt);
 		System.out.println(dec);
 		System.out.println(new Date(dec));
 
-		dec = simpleEncrypt.decryptTimestamp("373930333336373738373531322f91bc230437");
+		dec = simpleEncrypt.decryptTimestamp("-2if8vtdxg2x0ogjze46");
 		System.out.println(dec);
 		System.out.println(new Date(dec));
 	}
