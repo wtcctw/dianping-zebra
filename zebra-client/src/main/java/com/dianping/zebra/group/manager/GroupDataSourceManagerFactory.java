d52dfc4e7b6994da0928b010bc7fd63b1fbba876
Leo Liang <liangjinhua@gmail.com>
2014-03-20 19:29:24 +0800

    bug fix

--- zebra-client/src/main/java/com/dianping/zebra/group/manager/GroupDataSourceManagerFactory.java
+++ zebra-client/src/main/java/com/dianping/zebra/group/manager/GroupDataSourceManagerFactory.java
@@ -4,19 +4,10 @@ import com.dianping.zebra.group.config.DataSourceConfigManager;
 
 public class GroupDataSourceManagerFactory {
 
-	private static GroupDataSourceManager manager;
-
 	public static GroupDataSourceManager getGroupDataSourceManger(DataSourceConfigManager groupConfigManager,
 	      String connectionPoolType) {
-		if (manager == null) {
-			synchronized (GroupDataSourceManagerFactory.class) {
-				if (manager == null) {
-					manager = new C3P0GroupDataSourceManager(groupConfigManager);
-					manager.init();
-				}
-			}
-		}
-
+		C3P0GroupDataSourceManager manager = new C3P0GroupDataSourceManager(groupConfigManager);
+		manager.init();
 		return manager;
 	}
 }
