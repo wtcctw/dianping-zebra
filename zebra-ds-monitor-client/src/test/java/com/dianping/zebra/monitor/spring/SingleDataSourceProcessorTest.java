a0137a5f3bdb326af1bc3e373d6d2e6f3e661390
Dozer <mail@dozer.cc>
2014-07-10 23:37:49 +0800

    add c3p0 test

--- zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/spring/SingleDataSourceProcessorTest.java
+++ zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/spring/SingleDataSourceProcessorTest.java
@@ -1,5 +1,11 @@
 package com.dianping.zebra.monitor.spring;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.junit.*;
@@ -16,10 +22,55 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 public class SingleDataSourceProcessorTest {
 
 	@Autowired
-	private DataSource masterDs;
+	private ComboPooledDataSource masterDs;
 
 	@Test
-	public void test_c3p0_datasource_to_single_datasource() {
-		Assert.assertNotNull(masterDs);
+	@Ignore
+	public void test_c3p0_datasource_to_single_datasource()
+			throws InterruptedException, SQLException {
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						query();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						query();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+
+		Thread.sleep(3000);
+
+		 masterDs.setUser("test2");
+		 System.out.println("change username");
+		 masterDs.setPassword("test2");
+		 System.out.println("change password");
+
+		Thread.sleep(3000);
+	}
+
+	private void query() throws SQLException {
+		Connection conn = masterDs.getConnection();
+		Statement stmt = conn.createStatement();
+		ResultSet result = stmt.executeQuery("select @@read_only");
+		result.close();
+		stmt.close();
+		conn.close();
 	}
 }
