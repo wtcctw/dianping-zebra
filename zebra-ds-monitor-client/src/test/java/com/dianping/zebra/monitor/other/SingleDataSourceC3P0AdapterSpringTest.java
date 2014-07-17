28c78df749db6dc1893a63a69f2ada233c1561fd
Dozer <mail@dozer.cc>
2014-07-17 19:23:48 +0800

    remove debug code

--- zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/other/SingleDataSourceC3P0AdapterSpringTest.java
+++ zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/other/SingleDataSourceC3P0AdapterSpringTest.java
@@ -16,17 +16,17 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:config/spring/appcontext-test.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "classpath:config/spring/appcontext-test.xml")
 public class SingleDataSourceC3P0AdapterSpringTest {
 
-	@Resource(name = "dsTest")
-	private DataSource dsTest;
+	// @Resource(name = "dsTest")
+	// private DataSource dsTest;
 
 	@Test
 	@Ignore
 	public void test_jdbc() throws SQLException, PropertyVetoException, InterruptedException {
-		startThread();
+		// startThread();
 		// startThread();
 		// startThread();
 		// startThread();
@@ -50,33 +50,33 @@ public class SingleDataSourceC3P0AdapterSpringTest {
 		//
 		// dsTest.setDriverClass("asdsadasdasda.asdadas");
 
-		Thread.sleep(1000000);
+		// Thread.sleep(1000000);
 	}
 
-	private void startThread() throws InterruptedException {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					while (true) {
-						query();
-						Thread.yield();
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}).start();
-	}
-
-	private void query() throws SQLException {
-		Connection conn = dsTest.getConnection();
-		Statement stm = conn.createStatement();
-		ResultSet result = stm.executeQuery("select * from person");
-		Assert.assertTrue(result.next());
-		Assert.assertEquals("1", result.getString(1));
-		result.close();
-		stm.close();
-		conn.close();
-	}
+	// private void startThread() throws InterruptedException {
+	// new Thread(new Runnable() {
+	// @Override
+	// public void run() {
+	// try {
+	// while (true) {
+	// query();
+	// Thread.yield();
+	// }
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// }).start();
+	// }
+	//
+	// private void query() throws SQLException {
+	// Connection conn = dsTest.getConnection();
+	// Statement stm = conn.createStatement();
+	// ResultSet result = stm.executeQuery("select * from person");
+	// Assert.assertTrue(result.next());
+	// Assert.assertEquals("1", result.getString(1));
+	// result.close();
+	// stm.close();
+	// conn.close();
+	// }
 }
