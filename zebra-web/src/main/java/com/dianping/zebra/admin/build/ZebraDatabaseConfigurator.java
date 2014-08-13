696fe22d2f8ebed1b611700939eb29e756fd2f72
ainilife <ainilife@gmail.com>
2014-08-13 11:45:14 +0800

    add lion support to unidal-jdbc

--- zebra-web/src/main/java/com/dianping/zebra/admin/build/ZebraDatabaseConfigurator.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/build/ZebraDatabaseConfigurator.java
@@ -4,18 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.unidal.dal.jdbc.configuration.AbstractJdbcResourceConfigurator;
+import org.unidal.dal.jdbc.datasource.DataSource;
+import org.unidal.dal.jdbc.datasource.JdbcDataSourceConfigurationManager;
 import org.unidal.lookup.configuration.Component;
 
+import com.dianping.zebra.admin.datasource.JdbcDataSource;
+
 final class ZebraDatabaseConfigurator extends AbstractJdbcResourceConfigurator {
-   @Override
-   public List<Component> defineComponents() {
-      List<Component> all = new ArrayList<Component>();
+	@Override
+	public List<Component> defineComponents() {
+		List<Component> all = new ArrayList<Component>();
 
-      all.add(defineJdbcDataSourceComponent("zebra", "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/zebra", "root", "123456", "<![CDATA[useUnicode=true&characterEncoding=UTF-8&autoReconnect=true]]>"));
+		all.add(C(DataSource.class, "zebra", JdbcDataSource.class).req(JdbcDataSourceConfigurationManager.class).config(
+		      E("id").value("zebra"),
+		      E("maximum-pool-size").value("3"),
+		      E("connection-timeout").value("1s"),
+		      E("idle-timeout").value("10m"),
+		      E("statement-cache-size").value("1000"),
+		      E("properties").add(
+		            E("driver").value("com.mysql.jdbc.Driver"),
+		            E("URL").value("${zebra.server.jdbc.url}"),
+		            E("user").value("${zebra.server.jdbc.username}"),
+		            E("password").value("${zebra.server.jdbc.password}"),
+		            E("connectionProperties").value(
+		                  "<![CDATA[useUnicode=true&characterEncoding=UTF-8&autoReconnect=true]]>"))));
 
-      defineSimpleTableProviderComponents(all, "zebra", com.dianping.zebra.web.dal.stat._INDEX.getEntityClasses());
-      defineDaoComponents(all, com.dianping.zebra.web.dal.stat._INDEX.getDaoClasses());
+		defineSimpleTableProviderComponents(all, "zebra", com.dianping.zebra.web.dal.stat._INDEX.getEntityClasses());
+		defineDaoComponents(all, com.dianping.zebra.web.dal.stat._INDEX.getDaoClasses());
 
-      return all;
-   }
+		return all;
+	}
 }
