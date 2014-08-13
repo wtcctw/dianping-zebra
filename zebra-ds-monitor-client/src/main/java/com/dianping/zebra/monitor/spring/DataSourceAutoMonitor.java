b3e0ecfe8b517b8446dca05674475f6475991190
Dozer <mail@dozer.cc>
2014-08-13 14:12:35 +0800

    add upload data source info

--- zebra-ds-monitor-client/src/main/java/com/dianping/zebra/monitor/spring/DataSourceAutoMonitor.java
+++ zebra-ds-monitor-client/src/main/java/com/dianping/zebra/monitor/spring/DataSourceAutoMonitor.java
@@ -19,12 +19,14 @@ import com.dianping.cat.Cat;
 import com.dianping.lion.EnvZooKeeperConfig;
 import com.dianping.lion.client.ConfigCache;
 import com.dianping.lion.client.LionException;
+import com.dianping.zebra.group.exception.IllegalConfigException;
 import com.dianping.zebra.group.jdbc.GroupDataSource;
 import com.dianping.zebra.group.jdbc.SingleDataSource;
 import com.dianping.zebra.group.util.StringUtils;
 import com.dianping.zebra.monitor.sql.MonitorableDataSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.PropertyValue;
@@ -35,8 +37,14 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.core.Ordered;
 import org.springframework.core.PriorityOrdered;
 import org.springframework.util.ClassUtils;
+import org.unidal.helper.Files;
+import org.unidal.helper.Urls;
 
 import javax.sql.DataSource;
+import java.beans.PropertyDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -128,8 +136,9 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 	private GenericBeanDefinition createMonitorableBeanDefinition(String beanName, BeanDefinition dataSourceDefinition) {
 		String newBeanName = String.format("%s-z%d", beanName, nameId++);
 
-		if (dataSourceDefinition.getBeanClassName().equals(C3P0_CLASS_NAME)) {
-			JdbcInfo info = getBeanJdbcInfo(dataSourceDefinition);
+		DataSourceInfo info = getBeanJdbcInfo(dataSourceDefinition, beanName);
+
+		if (C3P0_CLASS_NAME.equals(info.getDataSourceBeanClass())) {
 			if ("mysql".equals(info.getType())) {
 				dataSourceDefinition.setBeanClassName(SingleDataSource.class.getName());
 				dataSourceDefinition.getConstructorArgumentValues().addIndexedArgumentValue(0, beanName);
@@ -138,11 +147,13 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 				if (dataSourceDefinition instanceof AbstractBeanDefinition) {
 					((AbstractBeanDefinition) dataSourceDefinition).setInitMethodName("init");
 				}
+
+				info.setReplaced(true);
 				Cat.logEvent("DAL.BeanFactory", String.format("Replace-%s(%s)", beanName, newBeanName));
 			} else {
 				Cat.logEvent("DAL.BeanFactory", String.format("NotMysql-%s(%s)", beanName, newBeanName));
 			}
-		} else if (dataSourceDefinition.getBeanClassName().equals(DPDL_CLASS_NAME)) {
+		} else if (DPDL_CLASS_NAME.equals(info.getDataSourceBeanClass())) {
 			BeanDefinition dpdlInnerDsBean = getDpdlInnerDsBean(dataSourceDefinition);
 			Set<PropertyValue> properties = getDpdlInnerDsPropertyValues(dpdlInnerDsBean);
 
@@ -158,6 +169,7 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 					((AbstractBeanDefinition) dataSourceDefinition).setInitMethodName("init");
 				}
 
+				info.setReplaced(true);
 				Cat.logEvent("DAL.BeanFactory", String.format("Replace-%s(%s)", beanName, newBeanName));
 			} else {
 				Cat.logEvent("DAL.BeanFactory",
@@ -168,6 +180,8 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 					String.format("Ignore-%s(%s)-%s", beanName, newBeanName, dataSourceDefinition.getBeanClassName()));
 		}
 
+		uploadDataSourceInfo(info);
+
 		listableBeanFactory.registerBeanDefinition(newBeanName, dataSourceDefinition);
 
 		GenericBeanDefinition monitorableDataSourceDefinition = new GenericBeanDefinition();
@@ -177,18 +191,34 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 		return monitorableDataSourceDefinition;
 	}
 
+	private void uploadDataSourceInfo(DataSourceInfo info) {
+		String url = getLionConfig("zebra.server.heartbeat.url");
+		if (StringUtils.isBlank(url)) {
+			logger.error("zebra.v2.datasource.upload.url not exists!");
+			Cat.logError(new IllegalConfigException("zebra.v2.datasource.upload.url not exists!"));
+		}
+
+		try {
+			callHttp(String.format("%s?%s", url, info.toString()));
+		} catch (IOException e) {
+			String msg = String.format("Call Zebra Web failed! [%s]", url);
+			logger.error(msg, e);
+			Cat.logError("msg", e);
+		}
+	}
+
 	private Set<PropertyValue> getDpdlInnerDsPropertyValues(BeanDefinition writeDsBean) {
 		Set<PropertyValue> properties = new HashSet<PropertyValue>();
 
 		if (writeDsBean.getBeanClassName().equals(C3P0_CLASS_NAME) || writeDsBean.getBeanClassName()
 				.equals(SingleDataSource.class.getName())) {
 
-			JdbcInfo info = getBeanJdbcInfo(writeDsBean);
+			DataSourceInfo info = getBeanJdbcInfo(writeDsBean, null);
 
 			if ("mysql".equals(info.getType())) {
-				String groupConfig = getLionConfig(String.format("groupds.%s.mapping", info.getDataBase()));
+				String groupConfig = getLionConfig(String.format("groupds.%s.mapping", info.getDatabase()));
 				if (!StringUtils.isBlank(groupConfig)) {
-					properties.add(new PropertyValue("jdbcRef", info.getDataBase()));
+					properties.add(new PropertyValue("jdbcRef", info.getDatabase()));
 
 					Set<String> ignoreList = getGroupDataSourceIgnoreProperties();
 					for (PropertyValue property : writeDsBean.getPropertyValues().getPropertyValues()) {
@@ -263,15 +293,29 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 		}
 	}
 
-	private JdbcInfo getBeanJdbcInfo(BeanDefinition bean) {
-		String url = ((TypedStringValue) bean.getPropertyValues().getPropertyValue("jdbcUrl").getValue())
-				.getValue().trim();
-		String user = ((TypedStringValue) bean.getPropertyValues().getPropertyValue("user").getValue()).getValue().trim();
+	private DataSourceInfo getBeanJdbcInfo(BeanDefinition bean, String beanName) {
+		DataSourceInfo info = new DataSourceInfo();
+		info.setDataSourceBeanName(beanName);
+		info.setDataSourceBeanClass(bean.getBeanClassName());
 
-		JdbcInfo info = getJdbcUrlInfo(isLionKey(url) ? getLionConfig(trimLionKey(url)) : url);
-		info.setUser(isLionKey(user) ? getLionConfig(trimLionKey(user)) : user);
+		getJdbcUrlInfo(info, getLionValueFromBean(bean, "jdbcUrl"));
+		info.setUsername(getLionValueFromBean(bean, "user"));
+		info.setInitPoolSize(getLionValueFromBean(bean, "initPoolSize"));
+		info.setMaxPoolSize(getLionValueFromBean(bean, "maxPoolSize"));
+		info.setMinPoolSize(getLionValueFromBean(bean, "minPoolSize"));
 
 		return info;
+	}
+
+	private String getLionValueFromBean(BeanDefinition bean, String key) {
+		try {
+			String value = ((TypedStringValue) bean.getPropertyValues().getPropertyValue(key).getValue())
+					.getValue().trim();
+
+			return isLionKey(value) ? getLionConfig(trimLionKey(value)) : value;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	private Set<String> getGroupDataSourceIgnoreProperties() {
@@ -283,49 +327,151 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 		return result;
 	}
 
-	private JdbcInfo getJdbcUrlInfo(String url) {
-		JdbcInfo info = new JdbcInfo();
+	private void getJdbcUrlInfo(DataSourceInfo info, String url) {
+		info.setUrl(url);
+
+		if (url == null) {
+			return;
+		}
+
 		Pattern p = Pattern.compile("^jdbc:([a-zA-Z0-9]+):\\/\\/([^\\/]+)\\/([^?]+).*$");
 		Matcher m = p.matcher(url);
 		if (m.find()) {
 			info.setType(m.group(1).toLowerCase());
-			info.setIp(m.group(2).toLowerCase());
-			info.setDataBase(m.group(3).toLowerCase());
+			info.setDatabase(m.group(3).toLowerCase());
 		}
-		return info;
 	}
 
-	static class JdbcInfo {
-		private String dataBase;
+	private String callHttp(String url) throws IOException {
+		InputStream inputStream = Urls.forIO().connectTimeout(1000).readTimeout(5000).openStream(url);
+		return Files.forIO().readFrom(inputStream, "utf-8");
+	}
 
-		private String user;
+	static class DataSourceInfo {
+		private static String app;
+
+		private static String ip;
+
+		private static String version;
+
+		private String dataSourceBeanName;
+
+		private String dataSourceBeanClass;
+
+		private String database;
+
+		private String url;
+
+		private String username;
+
+		private boolean replaced;
+
+		private String initPoolSize;
+
+		private String maxPoolSize;
+
+		private String minPoolSize;
 
 		private String type;
 
-		private String ip;
-
-		public String getUser() {
-			return user;
+		static {
+			//todo: read from phoenix env
 		}
 
-		public void setUser(String user) {
-			this.user = user;
+		public static String getApp() {
+			return app;
 		}
 
-		public String getIp() {
+		public static void setApp(String app) {
+			DataSourceInfo.app = app;
+		}
+
+		public static String getIp() {
 			return ip;
 		}
 
-		public void setIp(String ip) {
-			this.ip = ip;
+		public static void setIp(String ip) {
+			DataSourceInfo.ip = ip;
 		}
 
-		public String getDataBase() {
-			return dataBase;
+		public String getDataSourceBeanName() {
+			return dataSourceBeanName;
 		}
 
-		public void setDataBase(String dataBase) {
-			this.dataBase = dataBase;
+		public void setDataSourceBeanName(String dataSourceBeanName) {
+			this.dataSourceBeanName = dataSourceBeanName;
+		}
+
+		public String getDataSourceBeanClass() {
+			return dataSourceBeanClass;
+		}
+
+		public void setDataSourceBeanClass(String dataSourceBeanClass) {
+			this.dataSourceBeanClass = dataSourceBeanClass;
+		}
+
+		public String getDatabase() {
+			return database;
+		}
+
+		public void setDatabase(String database) {
+			this.database = database;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public boolean getReplaced() {
+			return replaced;
+		}
+
+		public void setReplaced(boolean replaced) {
+			this.replaced = replaced;
+		}
+
+		public String getInitPoolSize() {
+			return initPoolSize;
+		}
+
+		public void setInitPoolSize(String initPoolSize) {
+			this.initPoolSize = initPoolSize;
+		}
+
+		public String getMaxPoolSize() {
+			return maxPoolSize;
+		}
+
+		public void setMaxPoolSize(String maxPoolSize) {
+			this.maxPoolSize = maxPoolSize;
+		}
+
+		public String getMinPoolSize() {
+			return minPoolSize;
+		}
+
+		public void setMinPoolSize(String minPoolSize) {
+			this.minPoolSize = minPoolSize;
+		}
+
+		public String getVersion() {
+			return version;
+		}
+
+		public void setVersion(String version) {
+			this.version = version;
 		}
 
 		public String getType() {
@@ -334,6 +480,32 @@ public class DataSourceAutoMonitor implements BeanFactoryPostProcessor, Priority
 
 		public void setType(String type) {
 			this.type = type;
+		}
+
+		@Override public String toString() {
+			final StringBuffer sb = new StringBuffer();
+
+			PropertyDescriptor[] props = BeanUtils.getPropertyDescriptors(this.getClass());
+			for (PropertyDescriptor prop : props) {
+				if (prop.getName().equals("class")) {
+					continue;
+				}
+				try {
+					Object value = prop.getReadMethod().invoke(this);
+
+					if (value == null) {
+						continue;
+					}
+
+					if (sb.length() > 0) {
+						sb.append("&");
+					}
+					sb.append(prop.getName()).append("=")
+							.append(URLEncoder.encode(String.valueOf(value), "utf8"));
+				} catch (Exception e) {
+				}
+			}
+			return sb.toString();
 		}
 	}
 }
