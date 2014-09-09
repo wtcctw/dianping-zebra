66df63719ac0dff820ec88db54b5a8af9ecf611f
Dozer <mail@dozer.cc>
2014-09-09 09:43:18 +0800

    change filter loader

--- zebra-api/src/main/java/com/dianping/zebra/group/filter/DefaultFilterManager.java
+++ zebra-api/src/main/java/com/dianping/zebra/group/filter/DefaultFilterManager.java
@@ -1,44 +1,139 @@
 package com.dianping.zebra.group.filter;
 
-import com.dianping.zebra.group.Constants;
-import com.dianping.zebra.group.util.SpringBeanUtils;
 import com.dianping.zebra.group.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Dozer on 9/2/14.
  */
 public class DefaultFilterManager implements FilterManager {
-	public boolean hasSpringContext;
+	private static final String FILTER_KEY_NAME = "zebra.filter.";
+
+	private static final String FILTER_PROPERTY_NAME = "META-INF/zebra-filter.properties";
+
+	private final ConcurrentHashMap<String, String> aliasMap = new ConcurrentHashMap<String, String>();
+
+	private final ConcurrentHashMap<String, List<JdbcFilter>> cachedFilterMap = new ConcurrentHashMap<String, List<JdbcFilter>>();
 
 	public void init() {
 		try {
-			Class.forName("org.springframework.context.ApplicationContextAware");
-		} catch (ClassNotFoundException ignore) {
+			Properties filterProperties = loadFilterConfig();
+			for (Map.Entry<Object, Object> entry : filterProperties.entrySet()) {
+				String key = (String) entry.getKey();
+				if (key.startsWith(FILTER_KEY_NAME)) {
+					String name = key.substring(FILTER_KEY_NAME.length());
+					aliasMap.put(name, (String) entry.getValue());
+				}
+			}
+		} catch (Exception e) {
 		}
-		hasSpringContext = true;
 	}
 
-	@Override public JdbcFilter loadFilter(String filterBeanName) {
-		if (!hasSpringContext || StringUtils.isBlank(filterBeanName)) {
-			return new FilterWrapper();
+	private Class<?> loadClass(String className) {
+		Class<?> clazz = null;
+
+		if (className == null) {
+			return null;
 		}
 
+		try {
+			return Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			// skip
+		}
+
+		ClassLoader ctxClassLoader = Thread.currentThread().getContextClassLoader();
+		if (ctxClassLoader != null) {
+			try {
+				clazz = ctxClassLoader.loadClass(className);
+			} catch (ClassNotFoundException e) {
+				// skip
+			}
+		}
+
+		return clazz;
+	}
+
+	@Override public JdbcFilter loadFilter(String filterName) {
 		List<JdbcFilter> result = new ArrayList<JdbcFilter>();
-		String[] beanNames = filterBeanName.split(",");
-		for (String beanName : beanNames) {
-			Object tempBean = SpringBeanUtils.loadByName(Constants.ZEBRA_FILTER_PRFIX + beanName);
-			if (tempBean == null) {
-				continue;
+		String[] names = filterName.split(",");
+		for (String name : names) {
+			List<JdbcFilter> filters = loadFilterFromCache(filterName);
+			if (filters != null && filters.size() > 0) {
+				result.addAll(filters);
 			}
-			if (!(tempBean instanceof JdbcFilter)) {
-				continue;
-			}
-			result.add((JdbcFilter) tempBean);
+		}
+		return new FilterWrapper(result);
+	}
+
+	private void loadFilterConfig(Properties filterProperties, ClassLoader classLoader) throws IOException {
+		if (classLoader == null) {
+			return;
 		}
 
-		return new FilterWrapper(result);
+		for (Enumeration<URL> e = classLoader.getResources(FILTER_PROPERTY_NAME); e.hasMoreElements(); ) {
+			URL url = e.nextElement();
+
+			Properties property = new Properties();
+
+			InputStream is = null;
+			try {
+				is = url.openStream();
+				property.load(is);
+			} finally {
+				if (is != null) {
+					is.close();
+				}
+			}
+
+			filterProperties.putAll(property);
+		}
+	}
+
+	private Properties loadFilterConfig() throws IOException {
+		Properties filterProperties = new Properties();
+
+		loadFilterConfig(filterProperties, ClassLoader.getSystemClassLoader());
+		loadFilterConfig(filterProperties, Thread.currentThread().getContextClassLoader());
+
+		return filterProperties;
+	}
+
+	private List<JdbcFilter> loadFilterFromCache(String filterName) {
+		List<JdbcFilter> result = cachedFilterMap.get(filterName);
+		if (result != null) {
+			return result;
+		}
+
+		String filterClassNames = aliasMap.get(filterName);
+
+		if (StringUtils.isEmpty(filterClassNames)) {
+			return null;
+		}
+
+		result = new ArrayList<JdbcFilter>();
+
+		for (String filterClassName : filterClassNames.split(",")) {
+			Class<?> filterClass = loadClass(filterClassName);
+			if (filterClass == null) {
+				continue;
+			}
+
+			JdbcFilter filter;
+			try {
+				filter = (JdbcFilter) filterClass.newInstance();
+				result.add(filter);
+			} catch (InstantiationException e) {
+			} catch (IllegalAccessException e) {
+			}
+		}
+
+		cachedFilterMap.put(filterName, result);
+		return result;
 	}
 }
--- zebra-api/src/test/java/com/dianping/zebra/group/filter/DefaultFilterManagerTest.java
+++ zebra-api/src/test/java/com/dianping/zebra/group/filter/DefaultFilterManagerTest.java
@@ -10,8 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:config/spring/common/appcontext-zebra-api.xml")
 public class DefaultFilterManagerTest {
 
 	@Test
