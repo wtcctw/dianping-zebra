16d4ca14cb4078579cde55b9289b3210326859d3
Dozer <mail@dozer.cc>
2014-09-24 17:25:32 +0800

    add id to sql

--- zebra-api/src/main/java/com/dianping/zebra/group/filter/wall/WallFilter.java
+++ zebra-api/src/main/java/com/dianping/zebra/group/filter/wall/WallFilter.java
@@ -4,16 +4,46 @@ import com.dianping.zebra.group.filter.DefaultJdbcFilter;
 import com.dianping.zebra.group.filter.JdbcMetaData;
 import com.dianping.zebra.group.filter.delegate.FilterFunction;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by Dozer on 9/24/14.
  */
 public class WallFilter extends DefaultJdbcFilter {
-	@Override public int getOrder() {
+	private final static int MAX_LENGTH = 5;
+
+	private static String sha1(String input) throws NoSuchAlgorithmException {
+		MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+		byte[] result = mDigest.digest(input.getBytes());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < Math.min(MAX_LENGTH, result.length); i++) {
+			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		return sb.toString();
+	}
+
+	private String addIdToSql(String sql) {
+		try {
+			return String.format("%s/*%s*/", sql, generateId(sql));
+		} catch (NoSuchAlgorithmException e) {
+			return sql;
+		}
+	}
+
+	private String generateId(String sql) throws NoSuchAlgorithmException {
+		return sha1(sql);
+	}
+
+	public int getOrder() {
 		return MIN_ORDER;
 	}
 
 	@Override public <S> String sql(JdbcMetaData metaData, S source, FilterFunction<S, String> action) {
 		String result = super.sql(metaData, source, action);
+
+		result = addIdToSql(result);
 
 		System.out.println(result);
 
