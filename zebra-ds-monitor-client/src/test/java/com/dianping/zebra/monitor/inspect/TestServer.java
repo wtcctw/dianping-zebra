556638b84a9bd92d5630408eb33b257b0da6321f
Dozer <mail@dozer.cc>
2014-09-25 17:36:42 +0800

    ignore TestServer

--- zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/inspect/TestServer.java
+++ zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/inspect/TestServer.java
@@ -3,6 +3,7 @@ package com.dianping.zebra.monitor.inspect;
 import com.dianping.zebra.group.Constants;
 import com.dianping.zebra.group.jdbc.GroupDataSource;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 @RunWith(JUnit4.class)
+@Ignore
 public class TestServer extends JettyServer {
 	GroupDataSource ds;
 
