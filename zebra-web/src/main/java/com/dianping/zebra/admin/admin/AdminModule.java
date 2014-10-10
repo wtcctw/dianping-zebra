95c79720f8e35df1d06e557800114cb774044482
ainilife <ainilife@gmail.com>
2014-10-10 15:17:52 +0800

    fix zebra-web navi

--- zebra-web/src/main/java/com/dianping/zebra/admin/admin/AdminModule.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/admin/AdminModule.java
@@ -4,7 +4,7 @@ import org.unidal.web.mvc.AbstractModule;
 import org.unidal.web.mvc.annotation.ModuleMeta;
 import org.unidal.web.mvc.annotation.ModulePagesMeta;
 
-@ModuleMeta(name = "a", defaultInboundAction = "config", defaultTransition = "default", defaultErrorAction = "default")
+@ModuleMeta(name = "a", defaultInboundAction = "update", defaultTransition = "default", defaultErrorAction = "default")
 @ModulePagesMeta({
 
 com.dianping.zebra.admin.admin.page.update.Handler.class,
