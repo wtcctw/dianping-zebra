50291f988b05d2a52601fe915ac48f3b67ff94b5
ainilife <ainilife@gmail.com>
2014-11-07 13:45:51 +0800

    update version to 2.6.2

--- zebra-api/src/main/java/com/dianping/zebra/group/Constants.java
+++ zebra-api/src/main/java/com/dianping/zebra/group/Constants.java
@@ -10,7 +10,7 @@ package com.dianping.zebra.group;
  * @author Leo Liang, Dozer
  */
 public final class Constants {
-	public static final String ZEBRA_VERSION = "2.6.1";
+	public static final String ZEBRA_VERSION = "2.6.2";
 
 	public static final String SQL_FORCE_WRITE_HINT = "/*+zebra:w*/";
 
