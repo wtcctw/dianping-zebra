6536666e034fa80b1c06f3c7290de3db27243514
Dozer <mail@dozer.cc>
2015-01-26 10:54:55 +0800

    add jdbc class

---
+++ zebra-sharding/src/main/java/com/dianping/zebra/sharding/jdbc/ShardConnection.java
@@ -0,0 +1,9 @@
+package com.dianping.zebra.sharding.jdbc;
+
+/**
+ * Dozer @ 2015-01
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public class ShardConnection {
+}
---
+++ zebra-sharding/src/main/java/com/dianping/zebra/sharding/jdbc/ShardDataSource.java
@@ -0,0 +1,49 @@
+package com.dianping.zebra.sharding.jdbc;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Dozer @ 2015-01
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public class ShardDataSource implements DataSource {
+	@Override public <T> T unwrap(Class<T> iface) throws SQLException {
+		return null;
+	}
+
+	@Override public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return false;
+	}
+
+	@Override public Connection getConnection() throws SQLException {
+		return null;
+	}
+
+	@Override public Connection getConnection(String username, String password) throws SQLException {
+		return null;
+	}
+
+	public void init(){}
+
+	public void close(){}
+
+	@Override public PrintWriter getLogWriter() throws SQLException {
+		return null;
+	}
+
+	@Override public void setLogWriter(PrintWriter out) throws SQLException {
+
+	}
+
+	@Override public void setLoginTimeout(int seconds) throws SQLException {
+
+	}
+
+	@Override public int getLoginTimeout() throws SQLException {
+		return 0;
+	}
+}
