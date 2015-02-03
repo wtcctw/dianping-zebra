a3bca7eacd5fe2c3f6ce23a30e9d29c61e31c40c
ainilife <ainilife@gmail.com>
2015-02-03 17:16:03 +0800

    add AllTests for shard

---
+++ zebra-api/src/test/java/com/dianping/zebra/shard/AllTests.java
@@ -0,0 +1,19 @@
+package com.dianping.zebra.shard;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.dianping.zebra.shard.router.DataSourceRouterImplTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	
+	//router
+	DataSourceRouterImplTest.class
+	
+	
+})
+public class AllTests {
+
+}
