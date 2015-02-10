81acf85b042cc49eab1ced7ed77176e7ebb8a030
Dozer <mail@dozer.cc>
2015-02-10 14:44:52 +0800

    finish ReportServiceImpl

--- zebra-web/src/main/java/com/dianping/zebra/admin/controller/NotifyController.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/controller/NotifyController.java
@@ -1,21 +1,14 @@
 package com.dianping.zebra.admin.controller;
 
-import com.dianping.zebra.admin.dao.HeartbeatMapper;
 import com.dianping.zebra.admin.dto.HeartbeatDto;
 import com.dianping.zebra.admin.entity.HeartbeatEntity;
-import com.dianping.zebra.admin.service.DalConfigService;
-import com.dianping.zebra.admin.service.DalService;
-import com.dianping.zebra.admin.service.LogService;
-import com.google.common.base.Strings;
+import com.dianping.zebra.admin.service.ReportService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Date;
-import java.util.List;
 
 /**
  * Dozer @ 2015-02
@@ -27,19 +20,10 @@ import java.util.List;
 @RequestMapping(value = "/notify")
 public class NotifyController {
     @Autowired
-    private DalConfigService m_dalConfigService;
-    @Autowired
-    private DalService m_dalService;
-    @Autowired
-    private LogService m_logService;
-
-    @Autowired
     private ModelMapper mapper;
 
     @Autowired
-    private HeartbeatMapper heartbeatMapper;
-
-    private static final String NOT_FOUND = "N/A";
+    private ReportService reportService;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
@@ -47,47 +31,7 @@ public class NotifyController {
         if (model.getApp() != null && model.getIp() != null && model.getDataSourceBeanName() != null) {
 
             HeartbeatEntity entity = mapper.map(model, HeartbeatEntity.class);
-            if (!Strings.isNullOrEmpty(entity.getAppName())) {
-                entity.setAppName(entity.getAppName().toLowerCase());
-            }
-
-            if (Strings.isNullOrEmpty(entity.getDatabaseName())) {
-                entity.setDatabaseName(NOT_FOUND);
-            }
-            if (Strings.isNullOrEmpty(entity.getDatasourceBeanClass())) {
-                entity.setDatasourceBeanClass(NOT_FOUND);
-            }
-            if (Strings.isNullOrEmpty(entity.getVersion())) {
-                entity.setVersion(NOT_FOUND);
-            }
-            if (Strings.isNullOrEmpty(entity.getUsername())) {
-                entity.setUsername(NOT_FOUND);
-            }
-
-            if (!Strings.isNullOrEmpty(entity.getJdbcUrl())) {
-                String[] parts = entity.getJdbcUrl().split(":");
-                if (parts != null && parts.length > 2) {
-                    entity.setDatabaseType(parts[1].toLowerCase());
-                }
-            } else {
-                entity.setJdbcUrl(NOT_FOUND);
-                entity.setDatabaseType(NOT_FOUND);
-            }
-
-            List<HeartbeatEntity> old = heartbeatMapper.getHeartbeat(entity.getAppName(), entity.getIp(), entity.getDatasourceBeanName());
-
-            if (old.size() > 1) {
-                heartbeatMapper.deleteHeartbeat(entity.getAppName(), entity.getIp(), entity.getDatasourceBeanName());
-            } else if (old.size() == 1) {
-                entity.setId(old.get(0).getId());
-                entity.setCreateTime(old.get(0).getCreateTime());
-                entity.setUpdateTime(new Date());
-                heartbeatMapper.updateHeartbeat(entity);
-            } else {
-                entity.setUpdateTime(new Date());
-                entity.setCreateTime(new Date());
-                heartbeatMapper.insertHeartbeat(entity);
-            }
+            reportService.createOrUpdate(entity);
         }
         return null;
     }
--- zebra-web/src/main/java/com/dianping/zebra/admin/service/ReportService.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/service/ReportService.java
@@ -1,18 +1,18 @@
-//package com.dianping.zebra.admin.service;
-//
-//import com.dianping.zebra.admin.report.entity.App;
-//import com.dianping.zebra.admin.report.entity.Database;
-//import com.dianping.zebra.admin.report.entity.Report;
-//import com.dianping.zebra.web.dal.stat.Heartbeat;
-//
-//public interface ReportService {
-//
-//	public void createOrUpdate(Heartbeat heartbeat);
-//
-//	public Report getReport(boolean hasExtraInfo);
-//
-//	public App getApp(String app, boolean hasExtraInfo);
-//
-//	public Database getDatabase(String database, boolean hasExtraInfo);
-//
-//}
+package com.dianping.zebra.admin.service;
+
+import com.dianping.zebra.admin.dto.AppDto;
+import com.dianping.zebra.admin.dto.DatabaseDto;
+import com.dianping.zebra.admin.dto.ReportDto;
+import com.dianping.zebra.admin.entity.HeartbeatEntity;
+
+public interface ReportService {
+
+    public void createOrUpdate(HeartbeatEntity heartbeat);
+
+    public ReportDto getReport(boolean hasExtraInfo);
+
+    public AppDto getApp(String app, boolean hasExtraInfo);
+
+    public DatabaseDto getDatabase(String database, boolean hasExtraInfo);
+
+}
