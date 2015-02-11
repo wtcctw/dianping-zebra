1c085bcaf0817be93a28a07f68cc41372ca08d31
Dozer <mail@dozer.cc>
2015-02-11 17:58:40 +0800

    add alltest

--- zebra-api/src/test/java/com/dianping/zebra/shard/AllTests.java
+++ zebra-api/src/test/java/com/dianping/zebra/shard/AllTests.java
@@ -1,16 +1,40 @@
 package com.dianping.zebra.shard;
 
+import com.dianping.zebra.shard.config.XmlDataSourceRouterConfigLoaderTest;
+import com.dianping.zebra.shard.jdbc.*;
+import com.dianping.zebra.shard.parser.SelectTest;
 import com.dianping.zebra.shard.router.DataSourceRouterImplTest;
+import com.dianping.zebra.shard.router.rule.GroovyRuleEngineTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+        //config
+        XmlDataSourceRouterConfigLoaderTest.class,
+
+
+        //jdbc
+        ConnectionTest.class,
+        DataSourceTest.class,
+        MultiDBLifeCycleTest.class,
+        MultiDBPreparedStatementLifeCycleTest.class,
+        ResultSetTest.class,
+        SingleDBLifeCycleTest.class,
+        SingleDBPreparedStatementGroupFollowNoteIntegrationTest.class,
+        SingleDBPreparedStatementLifeCycleTest.class,
+        SpecialSQLTest.class,
+        StatementTest.class,
+
+        //parser
+        SelectTest.class,
 
         //router
         DataSourceRouterImplTest.class,
-        ShardSupportedCaseTest.class
+        ShardSupportedCaseTest.class,
+        GroovyRuleEngineTest.class
+
 
 })
 public class AllTests {
