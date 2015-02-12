8f86f13b74544037cb16518044c5a603b22ed1a7
ainilife <ainilife@gmail.com>
2015-02-12 11:08:33 +0800

    add ut for HidePasswordVisitor

--- zebra-api/src/test/java/com/dianping/zebra/group/config/DataSourceConfigManagerTest.java
+++ zebra-api/src/test/java/com/dianping/zebra/group/config/DataSourceConfigManagerTest.java
@@ -9,7 +9,6 @@ import org.xml.sax.SAXException;
 
 import com.dianping.zebra.group.config.DefaultDataSourceConfigManager.ReadOrWriteRole;
 import com.dianping.zebra.group.config.datasource.entity.DataSourceConfig;
-import com.dianping.zebra.group.config.datasource.entity.GroupDataSourceConfig;
 import com.dianping.zebra.group.config.datasource.transform.DefaultSaxParser;
 
 public class DataSourceConfigManagerTest {
@@ -23,35 +22,25 @@ public class DataSourceConfigManagerTest {
 	@Test
 	public void testConfig() throws SAXException, IOException {
 		DataSourceConfigManager dataSourceConfigManager = DataSourceConfigManagerFactory.getConfigManager("local",
-				"sample.ds.v2");
+		      "sample.ds.v2");
 		Map<String, DataSourceConfig> config = dataSourceConfigManager.getGroupDataSourceConfig().getDataSourceConfigs();
 
 		Map<String, DataSourceConfig> dataSourceConfigs = DefaultSaxParser.parse(
-				getClass().getClassLoader().getResourceAsStream("model/datasources.xml")).getDataSourceConfigs();
+		      getClass().getClassLoader().getResourceAsStream("model/datasources.xml")).getDataSourceConfigs();
 
 		for (DataSourceConfig entry : config.values()) {
 			DataSourceConfig dataSourceConfig = dataSourceConfigs.get(entry.getId());
-
 			Assert.assertEquals(dataSourceConfig.toString(), entry.toString());
 		}
-
-		System.out.println(dataSourceConfigManager.getGroupDataSourceConfig());
-
-		GroupDataSourceConfig groupDataSourceConfig = new GroupDataSourceConfig();
-		HidePasswordVisitor visitor = new HidePasswordVisitor(groupDataSourceConfig);
-		dataSourceConfigManager.getGroupDataSourceConfig().accept(visitor);
-		System.out.println("=====after hide the password=======");
-		System.out.println(groupDataSourceConfig);
-
 	}
 
 	@Test
 	public void testParseConfig() {
 		testAssert("(dianpingmobile-s1-read:1,dianpingmobile-s2-read:2),(dianpingmobile-m1-write)" //
-				, "{dianpingmobile-s1-read=r1, dianpingmobile-s2-read=r2, dianpingmobile-m1-write=w}");
+		      , "{dianpingmobile-s1-read=r1, dianpingmobile-s2-read=r2, dianpingmobile-m1-write=w}");
 		testAssert(
-				"(dianpingmobile-s1-read:1,    dianpingmobile-s2-read:1,   dianpingmobile-m1-write:1), (dianpingmobile-m1-write)"
-				//
-				, "{dianpingmobile-s1-read=r1, dianpingmobile-s2-read=r1, dianpingmobile-m1-write=wr1}");
+		      "(dianpingmobile-s1-read:1,    dianpingmobile-s2-read:1,   dianpingmobile-m1-write:1), (dianpingmobile-m1-write)"
+		      //
+		      , "{dianpingmobile-s1-read=r1, dianpingmobile-s2-read=r1, dianpingmobile-m1-write=wr1}");
 	}
 }
