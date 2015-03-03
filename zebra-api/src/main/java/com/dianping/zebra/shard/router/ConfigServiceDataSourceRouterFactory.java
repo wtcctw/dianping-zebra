8711b5d0a113fd68ec228f47203f4dc4695b7a3f
Dozer <mail@dozer.cc>
2015-03-03 17:14:12 +0800

    fix duplicate table name

--- zebra-api/src/main/java/com/dianping/zebra/shard/router/ConfigServiceDataSourceRouterFactory.java
+++ zebra-api/src/main/java/com/dianping/zebra/shard/router/ConfigServiceDataSourceRouterFactory.java
@@ -3,6 +3,8 @@ package com.dianping.zebra.shard.router;
 import com.dianping.zebra.config.ConfigService;
 import com.dianping.zebra.config.LionKey;
 import com.dianping.zebra.shard.config.RouterRuleConfig;
+import com.dianping.zebra.shard.config.TableShardDimensionConfig;
+import com.dianping.zebra.shard.config.TableShardRuleConfig;
 import com.dianping.zebra.shard.router.rule.RouterRule;
 import com.dianping.zebra.shard.router.rule.RouterRuleBuilder;
 import com.google.gson.Gson;
@@ -26,6 +28,16 @@ public class ConfigServiceDataSourceRouterFactory extends AbstractDataSourceRout
         this.configService = configService;
         this.ruleName = ruleName;
         this.routerConfig = new Gson().fromJson(configService.getProperty(LionKey.getShardConfigKey(ruleName)), RouterRuleConfig.class);
+
+        if (routerConfig.getTableShardConfigs() != null) {
+            for (TableShardRuleConfig tableConfig : routerConfig.getTableShardConfigs()) {
+                if (tableConfig.getDimensionConfigs() != null) {
+                    for (TableShardDimensionConfig dimensionConfig : tableConfig.getDimensionConfigs()) {
+                        dimensionConfig.setTableName(tableConfig.getTableName());
+                    }
+                }
+            }
+        }
     }
 
     @Override
