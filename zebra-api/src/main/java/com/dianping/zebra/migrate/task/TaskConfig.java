0e4f1ef3bbaf8670df058059fe3937b258687762
Dozer <mail@dozer.cc>
2015-03-04 11:17:38 +0800

    add getter setter

--- zebra-api/src/main/java/com/dianping/zebra/migrate/task/TaskConfig.java
+++ zebra-api/src/main/java/com/dianping/zebra/migrate/task/TaskConfig.java
@@ -18,4 +18,54 @@ public class TaskConfig {
 
     //limit 1000
     private int limitPerQuery;
+
+    private int sleepSecondsAfterPerQuery;
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+
+    public void setKeyName(String keyName) {
+        this.keyName = keyName;
+    }
+
+    public int getKeyStart() {
+        return keyStart;
+    }
+
+    public void setKeyStart(int keyStart) {
+        this.keyStart = keyStart;
+    }
+
+    public int getKeyEnd() {
+        return keyEnd;
+    }
+
+    public void setKeyEnd(int keyEnd) {
+        this.keyEnd = keyEnd;
+    }
+
+    public int getLimitPerQuery() {
+        return limitPerQuery;
+    }
+
+    public void setLimitPerQuery(int limitPerQuery) {
+        this.limitPerQuery = limitPerQuery;
+    }
+
+    public int getSleepSecondsAfterPerQuery() {
+        return sleepSecondsAfterPerQuery;
+    }
+
+    public void setSleepSecondsAfterPerQuery(int sleepSecondsAfterPerQuery) {
+        this.sleepSecondsAfterPerQuery = sleepSecondsAfterPerQuery;
+    }
 }
