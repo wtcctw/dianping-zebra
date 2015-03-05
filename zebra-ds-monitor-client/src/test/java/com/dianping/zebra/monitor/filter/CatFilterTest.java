e9d8d82aa539f7391e5977270e3f1004da3d6f91
ainilife <ainilife@gmail.com>
2015-03-05 14:57:21 +0800

    add back blacklist test case

--- zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/filter/CatFilterTest.java
+++ zebra-ds-monitor-client/src/test/java/com/dianping/zebra/monitor/filter/CatFilterTest.java
@@ -17,69 +17,83 @@ import com.dianping.zebra.group.jdbc.GroupDataSource;
 
 public class CatFilterTest {
 
-    @BeforeClass
-    public static void init() throws SQLException {
-   	  GroupDataSource ds = createDs();
+	@BeforeClass
+	public static void init() throws SQLException {
+		GroupDataSource ds = createDs();
 
-        Sql sql = new Sql(ds.getConnection());
-        sql.execute("CREATE TABLE Persons\n" + "(\n" + "Id int,\n" + "LastName varchar(255),\n"
-                + "FirstName varchar(255),\n" + "Address varchar(255),\n" + "City varchar(255)\n" + ")");
-        sql.execute("insert into persons (id,lastname,firstname,address,city) values (1,'','','','')");
-    }
+		Sql sql = new Sql(ds.getConnection());
+		sql.execute("CREATE TABLE Persons\n" + "(\n" + "Id int,\n" + "LastName varchar(255),\n"
+		      + "FirstName varchar(255),\n" + "Address varchar(255),\n" + "City varchar(255)\n" + ")");
+		sql.execute("insert into persons (id,lastname,firstname,address,city) values (1,'','','','')");
+	}
 
-    public static GroupDataSource createDs(){
-   	 GroupDataSource ds = new GroupDataSource();
-       ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
-       ds.setJdbcRef("sample.ds.v2");
-       ds.setFilter("!wall");
-       ds.init();
-       
-       return ds;
-    }
+	public static GroupDataSource createDs() {
+		GroupDataSource ds = new GroupDataSource();
+		ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
+		ds.setJdbcRef("sample.ds.v2");
+		ds.setFilter("!wall");
+		ds.init();
 
-    @Test(expected = SQLException.class, timeout = 30000)
-    public void test_connect_fail() throws SQLException {
-        GroupDataSource ds = new GroupDataSource();
-        ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
-        ds.setJdbcRef("sample.ds.error");
-        ds.setFilter("!wall,cat");
-        ds.init();
-        new Sql(ds.getConnection()).execute("select 1");
-    }
+		return ds;
+	}
 
-    @Test
-    public void test_connect_retry() throws SQLException {
-        GroupDataSource ds = new GroupDataSource();
-        ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
-        ds.setJdbcRef("sample.ds.retry");
-        ds.setFilter("cat");
-        ds.init();
-        
-        ExecutionContextHolder.getContext().add("sql_statement_name", "testPreparedStatementQuery");
+	@Test(expected = SQLException.class, timeout = 30000)
+	public void test_connect_fail() throws SQLException {
+		GroupDataSource ds = new GroupDataSource();
+		ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
+		ds.setJdbcRef("sample.ds.error");
+		ds.setFilter("!wall,cat");
+		ds.init();
+		new Sql(ds.getConnection()).execute("select 1");
+	}
 
-        new Sql(ds.getConnection()).execute("select 1");
-    }
-    
-    @Test
-    public void test_sql_success() throws SQLException {
-   	 GroupDataSource ds = createDs();
+	@Test
+	public void test_connect_retry() throws SQLException {
+		GroupDataSource ds = new GroupDataSource();
+		ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
+		ds.setJdbcRef("sample.ds.retry");
+		ds.setFilter("cat");
+		ds.init();
 
-        new Sql(ds.getConnection()).execute(Constants.SQL_FORCE_WRITE_HINT + "select * from Persons");
-    }
+		ExecutionContextHolder.getContext().add("sql_statement_name", "testPreparedStatementQuery");
 
-    @Test
-    public void test_sql_success1() throws SQLException {
-   	 GroupDataSource ds = createDs();
+		new Sql(ds.getConnection()).execute("select 1");
+	}
 
-        ExecutionContextHolder.getContext().add("sql_statement_name", "testPreparedStatementQuery");
+	@Test
+	public void test_sql_success() throws SQLException {
+		GroupDataSource ds = createDs();
 
-        new Sql(ds.getConnection()).execute(Constants.SQL_FORCE_WRITE_HINT + "select * from Persons");
-    }
+		new Sql(ds.getConnection()).execute(Constants.SQL_FORCE_WRITE_HINT + "select * from Persons");
+	}
 
-    @Test(expected = Exception.class)
-    public void test_sql_fail() throws SQLException {
-   	 GroupDataSource ds = createDs();
+	@Test
+	public void test_sql_success1() throws SQLException {
+		GroupDataSource ds = createDs();
 
-        new Sql(ds.getConnection()).execute("select * from xxx");
-    }
+		ExecutionContextHolder.getContext().add("sql_statement_name", "testPreparedStatementQuery");
+
+		new Sql(ds.getConnection()).execute(Constants.SQL_FORCE_WRITE_HINT + "select * from Persons");
+	}
+
+	@Test(expected = Exception.class)
+	public void test_sql_fail() throws SQLException {
+		GroupDataSource ds = createDs();
+
+		new Sql(ds.getConnection()).execute("select * from xxx");
+	}
+
+	@Test(expected = SQLException.class)
+	public void test_sql_rejected_by_flow_control() throws SQLException {
+		GroupDataSource ds = new GroupDataSource();
+		ds.setConfigManagerType(Constants.CONFIG_MANAGER_TYPE_LOCAL);
+		ds.setJdbcRef("sample.ds.v2");
+		ds.setFilter("wall,cat");
+		ds.init();
+
+		ExecutionContextHolder.getContext().add("sql_statement_name", "test");
+
+		new Sql(ds.getConnection()).execute("select 1", new Object[0]);
+	}
+
 }
