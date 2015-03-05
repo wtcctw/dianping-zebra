aa2a6b4b7ce62a2d3dd42c7c71a464ecd4241690
Dozer <mail@dozer.cc>
2015-03-05 10:52:26 +0800

    fix auth and test

--- zebra-web/src/main/java/com/dianping/zebra/admin/filter/AuthorizationFilter.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/filter/AuthorizationFilter.java
@@ -6,7 +6,6 @@ import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 /**
@@ -14,41 +13,34 @@ import java.io.IOException;
  */
 public class AuthorizationFilter implements Filter {
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
 
-	}
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-	      ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse rsp = (HttpServletResponse) response;
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse rsp = (HttpServletResponse) response;
 
-		String uri = req.getRequestURI();
-		String op = req.getParameter("op");
+        Cookie[] cookies = req.getCookies();
 
-		if ("/a/config".equalsIgnoreCase(uri) && "test".equalsIgnoreCase(op)) {
-			chain.doFilter(request, response);
-		} else {
-			Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("AuthorizationFilter")) {
+                    cookie.setMaxAge(60 * 30);
+                    rsp.addCookie(cookie);
+                    chain.doFilter(request, response);
+                    return;
+                }
+            }
+        }
+        rsp.setStatus(HttpResponseStatus.UNAUTHORIZED.getCode());
+    }
 
-			if (cookies != null) {
-				for (Cookie cookie : cookies) {
-					if (cookie.getName().equals("AuthorizationFilter")) {
-						cookie.setMaxAge(60 * 30);
-						rsp.addCookie(cookie);
-						chain.doFilter(request, response);
-						return;
-					}
-				}
-			}
-			rsp.setStatus(HttpResponseStatus.UNAUTHORIZED.getCode());
-		}
-	}
+    @Override
+    public void destroy() {
 
-	@Override
-	public void destroy() {
-
-	}
+    }
 }
