3bfed00c878ad4c5f9f48f7f659e5cb1cae89fa4
Dozer <mail@dozer.cc>
2015-03-10 18:45:19 +0800

    replace Collections.synchronizedSet to COncurrentHashMap

--- zebra-api/src/main/java/com/dianping/zebra/group/util/SqlAliasManager.java
+++ zebra-api/src/main/java/com/dianping/zebra/group/util/SqlAliasManager.java
@@ -1,68 +1,67 @@
 package com.dianping.zebra.group.util;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import com.dianping.avatar.tracker.ExecutionContextHolder;
 import com.dianping.zebra.util.StringUtils;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SqlAliasManager {
 
-	private static final String SQL_NAME = "sql_statement_name";
+    private static final String SQL_NAME = "sql_statement_name";
 
-	private static final String SQL_RETRY_SUFFIX = " (retry-by-zebra)";
+    private static final String SQL_RETRY_SUFFIX = " (retry-by-zebra)";
 
-	private static final ThreadLocal<String> sqlAlias = new ThreadLocal<String>();
+    private static final ThreadLocal<String> sqlAlias = new ThreadLocal<String>();
 
-	private static final int MAX_ALLOWED_SQL_CHAR = 1024;
+    private static final int MAX_ALLOWED_SQL_CHAR = 1024;
 
-	private static final int MAX_ALLOWED_TRUNCATED_SQL_NUM = 2000;
+    private static final int MAX_ALLOWED_TRUNCATED_SQL_NUM = 2000;
 
-	private static final Set<String> cachedTruncatedSqls = Collections.synchronizedSet(new HashSet<String>(
-	      MAX_ALLOWED_TRUNCATED_SQL_NUM));
+    private static final Map<String, String> cachedTruncatedSqls = new ConcurrentHashMap<String, String>(
+            MAX_ALLOWED_TRUNCATED_SQL_NUM);
 
-	public static String getAvatarSqlAlias() {
-		return ExecutionContextHolder.getContext().get(SQL_NAME);
-	}
+    public static String getAvatarSqlAlias() {
+        return ExecutionContextHolder.getContext().get(SQL_NAME);
+    }
 
-	private static String getCachedTruncatedSql(String sql) {
-		if (sql == null) {
-			return null;
-		}
+    private static String getCachedTruncatedSql(String sql) {
+        if (sql == null) {
+            return null;
+        }
 
-		if (sql.length() > MAX_ALLOWED_SQL_CHAR) {
-			sql = sql.substring(0, MAX_ALLOWED_SQL_CHAR);
-		}
+        if (sql.length() > MAX_ALLOWED_SQL_CHAR) {
+            sql = sql.substring(0, MAX_ALLOWED_SQL_CHAR);
+        }
 
-		if (!cachedTruncatedSqls.contains(sql) && cachedTruncatedSqls.size() > MAX_ALLOWED_TRUNCATED_SQL_NUM) {
-			return null;
-		} else {
-			cachedTruncatedSqls.add(sql);
-			return sql;
-		}
-	}
+        if (!cachedTruncatedSqls.containsKey(sql) && cachedTruncatedSqls.size() > MAX_ALLOWED_TRUNCATED_SQL_NUM) {
+            return null;
+        } else {
+            cachedTruncatedSqls.put(sql, null);
+            return sql;
+        }
+    }
 
-	public static String getSqlAlias() {
-		return sqlAlias.get();
-	}
+    public static String getSqlAlias() {
+        return sqlAlias.get();
+    }
 
-	public static void setRetrySqlAlias() {
-		String alias = sqlAlias.get();
+    public static void setRetrySqlAlias() {
+        String alias = sqlAlias.get();
 
-		if (alias != null) {
-			sqlAlias.set(alias + SQL_RETRY_SUFFIX);
-		}
-	}
+        if (alias != null) {
+            sqlAlias.set(alias + SQL_RETRY_SUFFIX);
+        }
+    }
 
-	public static void setSqlAlias(String sql) {
-		String sqlName = ExecutionContextHolder.getContext().get(SQL_NAME);
+    public static void setSqlAlias(String sql) {
+        String sqlName = ExecutionContextHolder.getContext().get(SQL_NAME);
 
-		if (StringUtils.isBlank(sqlName)) {
-			sqlAlias.set(getCachedTruncatedSql(sql));
-		} else {
-			sqlAlias.set(sqlName);
-		}
-	}
+        if (StringUtils.isBlank(sqlName)) {
+            sqlAlias.set(getCachedTruncatedSql(sql));
+        } else {
+            sqlAlias.set(sqlName);
+        }
+    }
 
 }
