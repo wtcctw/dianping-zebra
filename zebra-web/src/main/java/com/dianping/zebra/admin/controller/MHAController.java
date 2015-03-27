56689afeaf71f17e68dcdfb66d2f3aa8f40a6958
ainilife <ainilife@gmail.com>
2015-03-27 15:34:54 +0800

    delete unused file & add mhacontroller dto

--- zebra-web/src/main/java/com/dianping/zebra/admin/controller/MHAController.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/controller/MHAController.java
@@ -1,5 +1,6 @@
 package com.dianping.zebra.admin.controller;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,25 +22,67 @@ public class MHAController {
 	@RequestMapping(value = "/markdown", method = RequestMethod.GET)
 	@ResponseBody
 	public Object markdown(String ip, String port) {
+		MHAResultDto result = new MHAResultDto();
+
 		if (ip != null && port != null) {
 			Set<String> dsIds = mhaService.findDsIds(ip, port);
 
 			if (dsIds != null) {
 				mhaService.markDownDsIds(dsIds);
 			}
+
+			result.setDsIds(dsIds);
 		}
-		
-		return "success";
+
+		result.setStatus("success");
+
+		return result;
 	}
 
 	// 给zebra-web界面调用
 	@RequestMapping(value = "/markup", method = RequestMethod.GET)
 	@ResponseBody
 	public Object markup(String dsId) {
+		MHAResultDto result = new MHAResultDto();
+
 		if (dsId != null) {
 			mhaService.markUpDsId(dsId);
+
+			result.addDsId(dsId);
 		}
-		
-		return "success";
+
+		result.setStatus("success");
+
+		return result;
+	}
+
+	public class MHAResultDto {
+		private String status;
+
+		private Set<String> dsIds;
+
+		public String getStatus() {
+			return status;
+		}
+
+		public void setStatus(String status) {
+			this.status = status;
+		}
+
+		public Set<String> getDsIds() {
+			return dsIds;
+		}
+
+		public void setDsIds(Set<String> dsIds) {
+			this.dsIds = dsIds;
+		}
+
+		public void addDsId(String dsId) {
+			if (dsIds == null) {
+				dsIds = new HashSet<String>();
+			}
+
+			dsIds.add(dsId);
+		}
 	}
 }
