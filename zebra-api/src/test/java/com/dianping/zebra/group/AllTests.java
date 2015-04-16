4fdf3299eb146f75bc6a22461b0364df0c86f1db
ainilife <ainilife@gmail.com>
2015-04-16 11:01:07 +0800

    delete test case

--- zebra-api/src/test/java/com/dianping/zebra/group/AllTests.java
+++ zebra-api/src/test/java/com/dianping/zebra/group/AllTests.java
@@ -4,8 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.dianping.zebra.group.config.DataSourceConfigManagerTest;
 import com.dianping.zebra.config.LocalConfigServiceTest;
+import com.dianping.zebra.group.config.DataSourceConfigManagerTest;
 import com.dianping.zebra.group.config.SystemConfigManagerTest;
 import com.dianping.zebra.group.datasources.FailoverDataSourceTest;
 import com.dianping.zebra.group.datasources.LoadBalancedDataSourceTest;
@@ -22,7 +22,6 @@ import com.dianping.zebra.group.router.DpdlReadWriteStrategyImplTest;
 import com.dianping.zebra.group.router.GroupDataSourceRouterTest;
 import com.dianping.zebra.group.router.LocalContextReadWriteStrategyTest;
 import com.dianping.zebra.group.router.ReadWriteStrategyServiceLoaderTest;
-import com.dianping.zebra.group.util.AtomicRefreshTest;
 import com.dianping.zebra.group.util.SmoothReloadTest;
 import com.dianping.zebra.group.util.SqlUtilsTest;
 
@@ -58,7 +57,6 @@ import com.dianping.zebra.group.util.SqlUtilsTest;
 	  ReadWriteStrategyServiceLoaderTest.class,
 
 	  //util
-	  AtomicRefreshTest.class,
 	  SmoothReloadTest.class,
 	  SqlUtilsTest.class
 })
