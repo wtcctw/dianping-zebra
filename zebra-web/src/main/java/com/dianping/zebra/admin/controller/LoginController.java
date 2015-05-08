9c6902cb1d4e26b3ff25d689f90423a8727c3490
ainilife <ainilife@gmail.com>
2015-05-08 16:38:08 +0800

    let username & password into lion

--- zebra-web/src/main/java/com/dianping/zebra/admin/controller/LoginController.java
+++ zebra-web/src/main/java/com/dianping/zebra/admin/controller/LoginController.java
@@ -1,26 +1,34 @@
 package com.dianping.zebra.admin.controller;
 
-import com.dianping.zebra.admin.dto.LoginDto;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.dianping.zebra.admin.dto.LoginDto;
+import com.dianping.zebra.admin.service.LionService;
 
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
+	
+	@Autowired
+	private LionService lionService;
 
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
 	public String login(HttpServletRequest request, HttpServletResponse response, @RequestBody LoginDto loginDto)
 	      throws Exception {
-		if ("zebra".equals(loginDto.getUsername()) && "zebra".equals(loginDto.getPassword())) {
+		String user = lionService.getConfigFromZk("zebra.web.admin.user");
+		String passwd = lionService.getConfigFromZk("zebra.web.admin.password");
+		
+		if (user.equals(loginDto.getUsername()) && passwd.equals(loginDto.getPassword())) {
 			Cookie cookie = new Cookie("AuthorizationFilter", "");
 			cookie.setMaxAge(60 * 60 * 12); // 12 hours
 			cookie.setPath("/");
