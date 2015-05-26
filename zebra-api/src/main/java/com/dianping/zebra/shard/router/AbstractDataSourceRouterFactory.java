53f2acc10e785677398f15c9fb85927767f46c08
ainilife <ainilife@gmail.com>
2015-05-26 18:05:31 +0800

    fix create too many GroupDataSource

--- zebra-api/src/main/java/com/dianping/zebra/shard/router/AbstractDataSourceRouterFactory.java
+++ zebra-api/src/main/java/com/dianping/zebra/shard/router/AbstractDataSourceRouterFactory.java
@@ -1,26 +1,30 @@
 package com.dianping.zebra.shard.router;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import com.dianping.zebra.group.jdbc.GroupDataSource;
 import com.dianping.zebra.shard.config.RouterRuleConfig;
 import com.dianping.zebra.shard.config.TableShardDimensionConfig;
 import com.dianping.zebra.shard.config.TableShardRuleConfig;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class AbstractDataSourceRouterFactory implements DataSourceRouterFactory {
+	
 	protected Map<String, DataSource> getDataSourcePoolFromConfig(RouterRuleConfig config) {
 		Map<String, DataSource> pool = new HashMap<String, DataSource>();
 		for (TableShardRuleConfig table : config.getTableShardConfigs()) {
 			for (TableShardDimensionConfig dimension : table.getDimensionConfigs()) {
-				String[] dsNames = dimension.getDbIndexes().split(",");
-				for (String ds : dsNames) {
-					GroupDataSource groupDataSource = new GroupDataSource(ds);
-
-					groupDataSource.setForceWriteOnLogin(false); // HACK turn off
-					groupDataSource.init();
-					pool.put(ds, groupDataSource);
+				String[] jdbcRefs = dimension.getDbIndexes().split(",");
+				for (String jdbcRef : jdbcRefs) {
+					if(!pool.containsKey(jdbcRef)){
+						GroupDataSource groupDataSource = new GroupDataSource(jdbcRef);
+						
+						groupDataSource.setForceWriteOnLogin(false); // HACK turn off
+						groupDataSource.init();
+						pool.put(jdbcRef, groupDataSource);
+					}
 				}
 			}
 		}
