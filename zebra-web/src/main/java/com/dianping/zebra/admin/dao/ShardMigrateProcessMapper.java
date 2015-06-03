64604c610d829020461c39e998e1b047b2bb3a0c
Dozer <mail@dozer.cc>
2015-06-03 11:59:23 +0800

    add new dao

---
+++ zebra-web/src/main/java/com/dianping/zebra/admin/dao/ShardMigrateProcessMapper.java
@@ -0,0 +1,15 @@
+package com.dianping.zebra.admin.dao;
+
+import com.dianping.zebra.admin.entity.ShardMigrateProcessEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Dozer @ 6/3/15
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public interface ShardMigrateProcessMapper {
+    List<ShardMigrateProcessEntity> getByName(@Param(value = "name") String name);
+}
