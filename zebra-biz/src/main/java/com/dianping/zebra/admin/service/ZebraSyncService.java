e103613ebba6d42250beb5d60fe5a9cf9f5e1a4e
Dozer <mail@dozer.cc>
2015-06-04 13:57:22 +0800

    add dpsf

---
+++ zebra-biz/src/main/java/com/dianping/zebra/admin/service/ZebraSyncService.java
@@ -0,0 +1,9 @@
+package com.dianping.zebra.admin.service;
+
+/**
+ * Dozer @ 6/4/15
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public interface ZebraSyncService {
+}
---
+++ zebra-sync-service/src/main/java/com/dianping/zebra/admin/service/impl/ZebraSyncServiceImpl.java
@@ -0,0 +1,11 @@
+package com.dianping.zebra.admin.service.impl;
+
+import com.dianping.zebra.admin.service.ZebraSyncService;
+
+/**
+ * Dozer @ 6/4/15
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public class ZebraSyncServiceImpl implements ZebraSyncService {
+}
