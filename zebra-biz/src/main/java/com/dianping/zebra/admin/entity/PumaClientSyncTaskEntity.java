0a747a0b6ffeccf2a000df9619e92e9f91dca65d
ainilife <ainilife@gmail.com>
2015-06-09 15:46:30 +0800

    update SyncTaskEntity

--- zebra-biz/src/main/java/com/dianping/zebra/admin/entity/PumaClientSyncTaskEntity.java
+++ zebra-biz/src/main/java/com/dianping/zebra/admin/entity/PumaClientSyncTaskEntity.java
@@ -1,147 +1,188 @@
 package com.dianping.zebra.admin.entity;
 
+import java.util.Date;
+
 public class PumaClientSyncTaskEntity {
 
-    /**
-     * id
-     */
-    private int id;
+	/**
+	 * id
+	 */
+	private int id;
 
-    /**
-     * 分库分表规则
-     */
-    private String ruleName;
+	/**
+	 * 分库分表规则
+	 */
+	private String ruleName;
 
-    /**
-     * 逻辑表名
-     */
-    private String table;
+	/**
+	 * 逻辑表名
+	 */
+	private String table;
 
-    /**
-     * 数据库规则分配策略
-     */
-    private String dbRule;
+	/**
+	 * 数据库规则分配策略
+	 */
+	private String dbRule;
 
-    /**
-     * 数据库列表
-     */
-    private String dbIndexes;
+	/**
+	 * 数据库列表
+	 */
+	private String dbIndexes;
 
-    /**
-     * 表分配策略
-     */
-    private String tbRule;
+	/**
+	 * 表分配策略
+	 */
+	private String tbRule;
 
-    /**
-     * 表后缀名
-     */
-    private String tbSuffix;
+	/**
+	 * 表后缀名
+	 */
+	private String tbSuffix;
 
-    /**
-     * puma任务名
-     */
-    private String pumaTaskName;
+	/**
+	 * puma任务名
+	 */
+	private String pumaTaskName;
 
-    /**
-     * 监听的数据库
-     */
-    private String pumaDatabase;
+	/**
+	 * 监听的数据库
+	 */
+	private String pumaDatabase;
 
-    /**
-     * 监听的表，多个表用逗号分隔
-     */
-    private String pumaTables;
+	/**
+	 * 监听的表，多个表用逗号分隔
+	 */
+	private String pumaTables;
 
-    /**
-     * 执行任务的服务器
-     */
-    private String executor;
+	/**
+	 * 执行任务的服务器
+	 */
+	private String executor;
 
-    public int getId() {
-        return id;
-    }
+	/**
+	 * 状态： 1.创建 2.生效 3.暂停
+	 */
+	private String status;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	/**
+	 * 创建时间
+	 */
+	private Date createTime;
 
-    public String getRuleName() {
-        return ruleName;
-    }
+	/**
+	 * 更新时间
+	 */
+	private Date updateTime;
 
-    public void setRuleName(String ruleName) {
-        this.ruleName = ruleName;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getTable() {
-        return table;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setTable(String table) {
-        this.table = table;
-    }
+	public String getRuleName() {
+		return ruleName;
+	}
 
-    public String getDbRule() {
-        return dbRule;
-    }
+	public void setRuleName(String ruleName) {
+		this.ruleName = ruleName;
+	}
 
-    public void setDbRule(String dbRule) {
-        this.dbRule = dbRule;
-    }
+	public String getTable() {
+		return table;
+	}
 
-    public String getDbIndexes() {
-        return dbIndexes;
-    }
+	public void setTable(String table) {
+		this.table = table;
+	}
 
-    public void setDbIndexes(String dbIndexes) {
-        this.dbIndexes = dbIndexes;
-    }
+	public String getDbRule() {
+		return dbRule;
+	}
 
-    public String getTbRule() {
-        return tbRule;
-    }
+	public void setDbRule(String dbRule) {
+		this.dbRule = dbRule;
+	}
 
-    public void setTbRule(String tbRule) {
-        this.tbRule = tbRule;
-    }
+	public String getDbIndexes() {
+		return dbIndexes;
+	}
 
-    public String getTbSuffix() {
-        return tbSuffix;
-    }
+	public void setDbIndexes(String dbIndexes) {
+		this.dbIndexes = dbIndexes;
+	}
 
-    public void setTbSuffix(String tbSuffix) {
-        this.tbSuffix = tbSuffix;
-    }
+	public String getTbRule() {
+		return tbRule;
+	}
 
-    public String getPumaTaskName() {
-        return pumaTaskName;
-    }
+	public void setTbRule(String tbRule) {
+		this.tbRule = tbRule;
+	}
 
-    public void setPumaTaskName(String pumaTaskName) {
-        this.pumaTaskName = pumaTaskName;
-    }
+	public String getTbSuffix() {
+		return tbSuffix;
+	}
 
-    public String getExecutor() {
-        return executor;
-    }
+	public void setTbSuffix(String tbSuffix) {
+		this.tbSuffix = tbSuffix;
+	}
 
-    public void setExecutor(String executor) {
-        this.executor = executor;
-    }
+	public String getPumaTaskName() {
+		return pumaTaskName;
+	}
 
-    public String getPumaDatabase() {
-        return pumaDatabase;
-    }
+	public void setPumaTaskName(String pumaTaskName) {
+		this.pumaTaskName = pumaTaskName;
+	}
 
-    public void setPumaDatabase(String pumaDatabase) {
-        this.pumaDatabase = pumaDatabase;
-    }
+	public String getExecutor() {
+		return executor;
+	}
 
-    public String getPumaTables() {
-        return pumaTables;
-    }
+	public void setExecutor(String executor) {
+		this.executor = executor;
+	}
 
-    public void setPumaTables(String pumaTables) {
-        this.pumaTables = pumaTables;
-    }
+	public String getPumaDatabase() {
+		return pumaDatabase;
+	}
+
+	public void setPumaDatabase(String pumaDatabase) {
+		this.pumaDatabase = pumaDatabase;
+	}
+
+	public String getPumaTables() {
+		return pumaTables;
+	}
+
+	public void setPumaTables(String pumaTables) {
+		this.pumaTables = pumaTables;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 }
