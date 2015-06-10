ab634bb310f9d71ef6d7ff79b0ccf09936099e82
ainilife <ainilife@gmail.com>
2015-06-10 16:32:41 +0800

    change table name

--- zebra-biz/src/main/java/com/dianping/zebra/admin/dao/PumaClientSyncTaskMapper.java
+++ zebra-biz/src/main/java/com/dianping/zebra/admin/dao/PumaClientSyncTaskMapper.java
@@ -1,7 +1,6 @@
 package com.dianping.zebra.admin.dao;
 
 import com.dianping.zebra.admin.entity.PumaClientSyncTaskEntity;
-import com.dianping.zebra.admin.entity.ShardDumpTaskEntity;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
