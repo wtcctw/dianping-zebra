020011e2815df8e65e915d24fa51eb2e6ca3452c
Dozer <mail@dozer.cc>
2015-06-11 19:28:39 +0800

    change ve log

--- zebra-sync-service/src/main/java/com/dianping/zebra/admin/util/SqlGenerator.java
+++ zebra-sync-service/src/main/java/com/dianping/zebra/admin/util/SqlGenerator.java
@@ -19,7 +19,9 @@ public class SqlGenerator {
 	private static final VelocityEngine VE;
 
 	private static final Template it; // insert.
+
 	private static final Template dt; // delete.
+
 	private static final Template ut; // update.
 
 	static {
@@ -28,6 +30,7 @@ public class SqlGenerator {
 		VE.setProperty("class.resource.loader.class", ClasspathResourceLoader.class.getName());
 		VE.setProperty("class.resource.loader.cache", true);
 		VE.setProperty("class.resource.loader.modificationCheckInterval", "-1");
+		VE.setProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.Log4JLogChute");
 		VE.setProperty("input.encoding", "UTF-8");
 		VE.init();
 
