f5c989f27a9f24e1dabaad910f4e8206d26e3368
ainilife <ainilife@gmail.com>
2015-06-12 10:06:40 +0800

    fix bugs

--- zebra-sync-service/src/main/java/com/dianping/zebra/admin/job/ExecutorManager.java
+++ zebra-sync-service/src/main/java/com/dianping/zebra/admin/job/ExecutorManager.java
@@ -9,6 +9,7 @@ import com.dianping.zebra.admin.job.executor.ShardDumpTaskExecutor;
 import com.dianping.zebra.admin.service.ShardDumpService;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -21,10 +22,10 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.annotation.PostConstruct;
+
 /**
- * Dozer @ 6/2/15
- * mail@dozer.cc
- * http://www.dozer.cc
+ * Dozer @ 6/2/15 mail@dozer.cc http://www.dozer.cc
  */
 
 @Component
@@ -39,54 +40,55 @@ public class ExecutorManager {
 
 	private Map<Integer, ShardDumpTaskExecutor> shardDumpTaskExecutorMap = new ConcurrentHashMap<Integer, ShardDumpTaskExecutor>();
 
+	private String localAddress;
+
+	@PostConstruct
+	public void init() throws UnknownHostException {
+		this.localAddress = InetAddress.getLocalHost().getHostAddress();
+	}
+
 	@Scheduled(cron = "0/10 * * * * ?")
 	public synchronized void startPumaSyncTask() {
-		try {
-			List<PumaClientSyncTaskEntity> tasks = pumaClientSyncTaskMapper
-				.findEffectiveTaskByExecutor(InetAddress.getLocalHost().getHostAddress());
+		List<PumaClientSyncTaskEntity> tasks = pumaClientSyncTaskMapper.findEffectiveTaskByExecutor(this.localAddress);
 
-			for (PumaClientSyncTaskEntity task : tasks) {
-				if (pumaClientSyncTaskExecutorMap.containsKey(task.getId())) {
-					continue;
-				}
-
-				PumaClientSyncTaskExecutor executor = null;
-				try {
-					executor = new PumaClientSyncTaskExecutor(task);
-					executor.setPumaClientSyncTaskMapper(pumaClientSyncTaskMapper);
-					executor.init();
-					executor.start();
-					pumaClientSyncTaskExecutorMap.put(task.getId(), executor);
-				} catch (Exception e) {
-					if (executor != null) {
-						executor.stop();
-					}
-				}
-
+		for (PumaClientSyncTaskEntity task : tasks) {
+			if (pumaClientSyncTaskExecutorMap.containsKey(task.getId())) {
+				continue;
 			}
 
-			Set<Integer> idToRemove = new HashSet<Integer>();
-			for (int id : pumaClientSyncTaskExecutorMap.keySet()) {
-				final int finalId = id;
-				if (Iterables.all(tasks, new Predicate<PumaClientSyncTaskEntity>() {
-					@Override
-					public boolean apply(PumaClientSyncTaskEntity entity) {
-						return entity.getId() != finalId;
-					}
-				})) {
-					idToRemove.add(finalId);
+			PumaClientSyncTaskExecutor executor = null;
+			try {
+				executor = new PumaClientSyncTaskExecutor(task);
+				executor.setPumaClientSyncTaskMapper(pumaClientSyncTaskMapper);
+				executor.init();
+				executor.start();
+				pumaClientSyncTaskExecutorMap.put(task.getId(), executor);
+			} catch (Exception e) {
+				if (executor != null) {
+					executor.stop();
 				}
 			}
 
-			for (int id : idToRemove) {
-				ShardDumpTaskExecutor task = shardDumpTaskExecutorMap.remove(id);
-				if (task != null) {
-					task.stop();
-				}
-			}
+		}
 
-		} catch (UnknownHostException e) {
-			Cat.logError(e);
+		Set<Integer> idToRemove = new HashSet<Integer>();
+		for (int id : pumaClientSyncTaskExecutorMap.keySet()) {
+			final int finalId = id;
+			if (Iterables.all(tasks, new Predicate<PumaClientSyncTaskEntity>() {
+				@Override
+				public boolean apply(PumaClientSyncTaskEntity entity) {
+					return entity.getId() != finalId;
+				}
+			})) {
+				idToRemove.add(finalId);
+			}
+		}
+
+		for (int id : idToRemove) {
+			ShardDumpTaskExecutor task = shardDumpTaskExecutorMap.remove(id);
+			if (task != null) {
+				task.stop();
+			}
 		}
 	}
 
