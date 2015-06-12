a7acae70652f255471917b5182eb4b709b729e17
Dozer <mail@dozer.cc>
2015-06-12 16:49:35 +0800

    add new test

--- zebra-api/src/main/java/com/dianping/zebra/shard/router/rule/engine/RuleEngineBase.java
+++ zebra-api/src/main/java/com/dianping/zebra/shard/router/rule/engine/RuleEngineBase.java
@@ -9,13 +9,13 @@ import java.util.zip.CRC32;
  * http://www.dozer.cc
  */
 public class RuleEngineBase {
-    public long crc32(Object str) throws UnsupportedEncodingException {
-        return crc32(str, "utf-8");
-    }
+	public long crc32(Object str) throws UnsupportedEncodingException {
+		return crc32(str, "utf-8");
+	}
 
-    public long crc32(Object str, String encode) throws UnsupportedEncodingException {
-        CRC32 crc32 = new CRC32();
-        crc32.update(String.valueOf(str).getBytes(encode));
-        return crc32.getValue();
-    }
+	public long crc32(Object str, String encode) throws UnsupportedEncodingException {
+		CRC32 crc32 = new CRC32();
+		crc32.update(String.valueOf(str).getBytes(encode));
+		return crc32.getValue();
+	}
 }
