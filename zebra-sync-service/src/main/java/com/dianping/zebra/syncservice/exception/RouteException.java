07999605023e7cbdaea9fbbe57e413f9ad8eb9e6
ainilife <ainilife@gmail.com>
2015-08-19 10:31:20 +0800

    format code

--- zebra-sync-service/src/main/java/com/dianping/zebra/syncservice/exception/RouteException.java
+++ zebra-sync-service/src/main/java/com/dianping/zebra/syncservice/exception/RouteException.java
@@ -1,11 +1,11 @@
 package com.dianping.zebra.syncservice.exception;
 
-/**
- * Dozer @ 6/17/15
- * mail@dozer.cc
- * http://www.dozer.cc
- */
 public class RouteException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 807361358228116711L;
+
 	public RouteException(Exception e) {
 		super(e);
 	}
