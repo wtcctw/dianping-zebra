e62523e75b88b6197e1c826ad43d04da832312d5
ainilife <ainilife@gmail.com>
2016-03-14 14:27:45 +0800

    add ut

--- zebra-api/src/test/java/com/dianping/zebra/shard/router/rule/SimpleDataSourceProviderTest.java
+++ zebra-api/src/test/java/com/dianping/zebra/shard/router/rule/SimpleDataSourceProviderTest.java
@@ -5,13 +5,10 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 import static org.junit.Assert.assertArrayEquals;
 
-/**
- * Dozer @ 7/3/15
- * mail@dozer.cc
- * http://www.dozer.cc
- */
 public class SimpleDataSourceProviderTest {
 	@Test
 	public void test_split_db() throws Exception {
@@ -24,7 +21,29 @@ public class SimpleDataSourceProviderTest {
 		SimpleDataSourceProvider target = new SimpleDataSourceProvider("UOD_Order","unifiedorder_operation","alldb:[_Operation0,_Operation0]","0");
 	
 		Map<String, Set<String>> allDBAndTables = target.getAllDBAndTables();
+		Assert.assertEquals(1, allDBAndTables.size());
+		Assert.assertEquals(1, allDBAndTables.get("unifiedorder_operation").size());
+		Assert.assertEquals(true, allDBAndTables.get("unifiedorder_operation").contains("UOD_Order_Operation0"));
 		
-		System.out.println(allDBAndTables);
+		DataSourceBO dataSource = target.getDataSource(0);
+		Assert.assertEquals(1, dataSource.getPhysicalTables().size());
+	}
+	
+	@Test
+	public void test_rule_parser() throws Exception {
+		SimpleDataSourceProvider target = new SimpleDataSourceProvider("UOD_Order","a,b_[2-4]","alldb:[_Operation0,_Operation31]","0");
+	
+		Map<String, Set<String>> allDBAndTables = target.getAllDBAndTables();
+		Assert.assertEquals(4, allDBAndTables.size());
+		Assert.assertEquals(8, allDBAndTables.get("a").size());
+		Assert.assertEquals(8, allDBAndTables.get("b_2").size());
+		Assert.assertEquals(8, allDBAndTables.get("b_3").size());
+		Assert.assertEquals(8, allDBAndTables.get("b_4").size());
+		
+		DataSourceBO dataSource = target.getDataSource(0);
+		Assert.assertEquals(8, dataSource.getPhysicalTables().size());
+		Assert.assertTrue(dataSource.getPhysicalTables().contains("UOD_Order_Operation0"));
+		Assert.assertTrue(dataSource.getPhysicalTables().contains("UOD_Order_Operation7"));
+		Assert.assertFalse(dataSource.getPhysicalTables().contains("UOD_Order_Operation8"));
 	}
 }
